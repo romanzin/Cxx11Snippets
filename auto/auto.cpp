bfd46dc4fb17ff7e615ecaca210d24cb2b64b920
Roman Zinchenko <rzinchenko@opendesign.com>
2018-12-07 03:55:59 +1000

    new features

--- auto/auto.cpp
+++ auto/auto.cpp
@@ -3,9 +3,42 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+template <typename A, typename B>
+auto additive(A a, B b) -> decltype(a + b) 
+{
+  return a + b;
+}
 
 int main()
 {
-    return 0;
+  /* Simple variable */
+  auto i = 0x555;
+
+  auto ptr = nullptr;
+
+  /* Container value */
+
+  std::map<std::string, std::vector<std::string>> map = { { "x0", { { "Alpha" }, { "Beta" } } } , { "y0",{ { "Gamma" },{ "Delta" } } } };
+
+  for (auto &it1 = map.begin(); it1 != map.end(); ++it1)
+  {
+    for (auto &it2 = std::begin(it1->second); it2 != std::end(it1->second); ++it2)
+    {
+      std::cout << *it2 << " ";
+    }
+
+    std::cout << "\n";
+  }
+
+  auto add = additive(0, 0.5);
+
+  std::cout << add << "\n";
+
+  return 0;
 }
 
