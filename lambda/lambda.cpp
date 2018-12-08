f99c09bc4a3ad81c5cd51ce7245515bb5cf25653
Roman Zinchenko <rzinchenko@opendesign.com>
2018-12-09 00:27:13 +1000

    lambda

--- lambda/lambda.cpp
+++ lambda/lambda.cpp
@@ -3,9 +3,23 @@
 
 #include "stdafx.h"
 
-
 int main()
 {
-    return 0;
+  std::vector<int> v = { 1, 2, 3 };
+
+  std::for_each(std::begin(v), std::end(v), [](int n) { std::cout << n << std::endl; });
+
+  auto is_odd = [](int n) {return n % 2 == 1; };
+  auto pos = std::find_if(std::begin(v), std::end(v), is_odd);
+  if (pos != std::end(v))
+  {
+    std::cout << *pos << std::endl;
+  }
+
+  std::function<int(int)> fib = [&fib](int n) { return n < 2 ? 1 : fib(n - 1) + fib(n - 2); };
+
+  std::cout << fib(5) << std::endl;
+
+  return 0;
 }
 
--- lambda/stdafx.h
+++ lambda/stdafx.h
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #include <tchar.h>
 
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 // TODO: reference additional headers your program requires here
